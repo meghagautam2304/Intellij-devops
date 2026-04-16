0c6f9522658ccd957621838a4e2cff483665f7cd
Megha Gautam <meghagautam2304@gmail.com>
2026-04-16 21:02:27 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int n=5;
+        if(n%2==0){
+            System.out.println(n+" is even number");
+        }
+        else{
+            System.out.println(n+" is odd number");
+        }
+    }
+}
